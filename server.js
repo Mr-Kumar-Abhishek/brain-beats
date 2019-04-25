b0e2710660e5efcea01b64d4a7189d64bffb35f0
Abhishek Kumar <mr.kumar.abhishek@email.com>
2019-04-25 11:15:16 +0000

    set basic template for node.js app
    😼💪 Checkpoint
    ./package.json:2623939/1353
    
    🌕💺 Checkpoint
    ./server.js:2623939/195
    
    👗🎺 Checkpoint
    ./package.json:2458109/1101
    ./server.js:3091723/1
    
    🌽🏡 Checkpoint
    ./views/index.html:2304451/48
    ./package.json:197979/270:514314/176:2304451/36
    ./server.js:514314/1
    ./README.md:514314/135
    
    🎢🌺 Checkpoint
    ./.gitconfig:514314/58
    ./README.md:514314/121
    ./server.js:197979/33:1893008/333
    
    set basic template for node.js app

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ app.set('port', port);
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+  app.get('/', async (req, res) => {
+    res.sendFile(`${__dirname}/views/index.html`);
+  });
 
+
+app.listen(port);
