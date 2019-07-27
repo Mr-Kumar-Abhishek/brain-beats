efaca6cbb3cf9167b080355baf39452d131d9415
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2019-07-27 06:13:27 -0400

    start with ssl and non ssl ports along with web manifest

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
-// server.js
-// where the node app starts
 
-// init project
-const http = require('http');
-const https = require('https');
+
+var fs = require("fs");
+var http = require("http");
+var https = require('https');
 var debug = require('debug')('test');
 var express = require('express');
 var normalizePort = require( 'normalize-port' );
 var app = express();
 
+var privateKey = fs.readFileSync('/home/kumar/brain-beats-certs/live/brain-beats.tk/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/home/kumar/brain-beats-certs/live/brain-beats.tk/cert.pem', 'utf8');
+var ca = fs.readFileSync('/home/kumar/brain-beats-certs/live/brain-beats.tk/chain.pem', 'utf8');
 
-/**
- * Get port from environment and store in Express.
- */
 
-var port = normalizePort(process.env.PORT || '8080');
-app.set('port', port);
+var credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
-// http://expressjs.com/en/starter/static-files.html
+
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
   app.get('/', async (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`);
   });
 
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
-app.listen(port);
+httpServer.listen(8080, () => {
+	console.log('HTTP Server running on port 8080');
+});
+
+httpsServer.listen(8443, () => {
+	console.log('HTTPS Server running on port 8443');
+});
