9558d13820f942de273db49c7a673090786a41dc
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2019-08-12 00:09:49 -0400

    include start and stop functionality of sample audio

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,19 @@
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
 // create Oscillator node
-var oscillator = audioCtx.createOscillator();
 
-oscillator.type = 'sine';
-oscillator.frequency.setValueAtTime(24, audioCtx.currentTime); // value in hertz
-oscillator.connect(audioCtx.destination);
+var oscillator;
 
+function play_sample() {
+
+ oscillator = audioCtx.createOscillator();
+
+ oscillator.type = 'sine';
+ oscillator.frequency.setValueAtTime(24, audioCtx.currentTime); // value in hertz
+ oscillator.connect(audioCtx.destination);
+ oscillator.start();
+}
+
+function stop_sample(){
+ oscillator.stop();
+}
