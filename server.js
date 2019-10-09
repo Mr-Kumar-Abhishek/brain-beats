6fad3109f6df568b8ce8d0e0d00c179b8d8d0398
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2019-10-09 16:37:17 +0000

    remove configuration of ssl

--- server.js
+++ server.js
@@ -8,17 +8,6 @@ var express = require('express');
 var normalizePort = require( 'normalize-port' );
 var app = express();
 
-var privateKey = fs.readFileSync('/home/kumar/brain-beats-certs/live/brain-beats.tk/privkey.pem', 'utf8');
-var certificate = fs.readFileSync('/home/kumar/brain-beats-certs/live/brain-beats.tk/cert.pem', 'utf8');
-var ca = fs.readFileSync('/home/kumar/brain-beats-certs/live/brain-beats.tk/chain.pem', 'utf8');
-
-
-var credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
-
 
 app.use(express.static('public'));
 
@@ -26,13 +15,10 @@ app.use(express.static('public'));
     res.sendFile(`${__dirname}/views/index.html`);
   });
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(8080, () => {
+app.listen(8080, () => {
 	console.log('HTTP Server running on port 8080');
 });
 
-httpsServer.listen(8443, () => {
+app.listen(8443, () => {
 	console.log('HTTPS Server running on port 8443');
 });
