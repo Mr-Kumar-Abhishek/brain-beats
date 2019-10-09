48e92ed5035ac74906aa078c2f46150872ceeb0e
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2019-10-09 17:43:49 +0000

    👬🌓 Checkpoint ./public/js/main.js:197979/418

--- public/js/main.js
+++ public/js/main.js
@@ -7,12 +7,11 @@ var oscillator;
 
 function play_sample() {
 
- oscillator = audioCtx.createOscillator();
+//create a synth and connect it to the master output (your speakers)
+var synth = new Tone.Synth().toMaster();
 
- oscillator.type = 'sine';
- oscillator.frequency.setValueAtTime(24, audioCtx.currentTime); // value in hertz
- oscillator.connect(audioCtx.destination);
- oscillator.start();
+//play a middle 'C' for the duration of an 8th note
+synth.triggerAttackRelease("C4", "8n");
 }
 
 function stop_sample(){
