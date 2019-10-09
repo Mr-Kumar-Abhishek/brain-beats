fe1522f9adf0b312ec6e131f9863ec8163c8fbd1
Abhishek Kumar <mr.kumar.abhishek@email.com>
2019-10-09 17:48:38 +0000

    remove node.js

--- js/main.js
+++ js/main.js
@@ -7,11 +7,12 @@ var oscillator;
 
 function play_sample() {
 
-//create a synth and connect it to the master output (your speakers)
-var synth = new Tone.Synth().toMaster();
+ oscillator = audioCtx.createOscillator();
 
-//play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease("C4", "8n");
+ oscillator.type = 'sine';
+ oscillator.frequency.setValueAtTime(24, audioCtx.currentTime); // value in hertz
+ oscillator.connect(audioCtx.destination);
+ oscillator.start();
 }
 
 function stop_sample(){
