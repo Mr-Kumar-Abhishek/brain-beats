066f16acf2f35b06413dd4a040a606d8a081a885
Abhishek Kumar <mr.kumar.abhishek@email.com>
2019-10-11 05:19:22 +0000

    🍥🦂 Checkpoint ./js/main.js:197979/1044

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 // create Oscillator node
 
 var oscillator;
-var monaural_oscilator_1;
-var monaural_oscilator_2;
+var monaural_oscillator_1;
+var monaural_oscillator_2;
 
 var solfeggio_flag = 0;
 var monaural_flag = 0;
+var oscillator_type = 'sine'
 
 function play_solfeggio(freq) {
 
@@ -27,13 +28,41 @@ function play_solfeggio(freq) {
   }
 }
 
-function play_monaural(freq1, freq1){
-  if ()  
+function play_monaural(freq1, freq2){
+  if (monaural_flag == 0){
+    monaural_flag = 1;
+    
+    monaural_oscillator_1 = audioCtx.createOscillator();
+    monaural_oscillator_2 = audioCtx.createOscillator();
+    
+    monaural_oscillator_1.type = oscillator_type;
+    monaural_oscillator_2.type = oscillator_type;
+    
+    monaural_oscillator_1.frequency.setValueAtTime(freq1, audioCtx.currentTime);
+    monaural_oscillator_2.frequency.setValueAtTime(freq2, audioCtx.currentTime);
+    
+    monaural_oscillator_1.connect(audioCtx.destination);
+    monaural_oscillator_2.connect(audioCtx.destination);
+    monaural_oscillator_1.start();
+    monaural_oscillator_2.start();
+  }else {
+    monaural_stop();
+    
+    
+  }
 }
 
 function stop_solfeggio(){
   if (solfeggio_flag == 1){
     solfeggio_flag = 0;
     oscillator.stop(); 
+  }
+}
+
+function stop_monaural(){
+  if(monaural_flag == 1){
+    monaural_flag = 0;
+    monaural_oscillator_2.stop();
+    monaural_oscillator_1.stop();
   }
 }
