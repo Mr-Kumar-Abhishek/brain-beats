b1875cab8fa3bf2e3ace2578d18c5248426ee016
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2019-10-13 11:11:54 +0000

    remove scope

--- js/pwabuilder-sw-register.js
+++ js/pwabuilder-sw-register.js
@@ -9,9 +9,7 @@ if ("serviceWorker" in navigator) {
   } else {
     // Register the service worker
     navigator.serviceWorker
-      .register("pwabuilder-sw.js", {
-        scope: "./"
-      })
+      .register("js/pwabuilder-sw.js")
       .then(function (reg) {
         console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
       });
