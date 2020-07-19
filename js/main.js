930e07e400453b5a7ff9e1f621fb0d86fbd11a13
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2020-07-19 10:56:48 +0530

    remove old range slider functions

--- js/main.js
+++ js/main.js
@@ -216,28 +216,6 @@ function warning(whichy){
 }
 
 
-// css range slider 
-
-var rangeSlider = function(){
-  var slider = $('.range-slider'),
-      range = $('.range-slider__range'),
-      value = $('.range-slider__value');
-    
-  slider.each(function(){
-
-    value.each(function(){
-      var value = $(this).prev().attr('value');
-      $(this).html(value);
-    });
-
-    range.on('input', function(){
-      $(this).next(value).html(this.value);
-    });
-  });
-};
-
-rangeSlider();
-
 function live_volume_set(){
   console.log("live volume ran");
   
