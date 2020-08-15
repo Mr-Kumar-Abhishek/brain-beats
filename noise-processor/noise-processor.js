22493d7a68b7cbe54d77a629e2ab92932bfb4e1e
Abhishek Kumar <mr.kumar.abhishek@email.com>
2020-08-15 21:39:25 +0530

    bug fix to make white noise

--- noise-processor/noise-processor.js
+++ noise-processor/noise-processor.js
@@ -1,7 +1,10 @@
-const audioContext = new AudioContext()
-await audioContext.audioWorklet.addModule('white-noise-processor.js')
-const whiteNoiseNode = new AudioWorkletNode(audioContext, 'white-noise-processor');
-const whiteNoiseNodeGain = audioContext.createGain();
-whiteNoiseNodeGain.gain.value = 0.0;
-whiteNoiseNode.connect(whiteNoiseNodeGain);
-whiteNoiseNodeGain.connect(audioContext.destination);
+
+async function white_noise_process(val) {
+    const audioContext = new AudioContext();
+    await audioContext.audioWorklet.addModule('noise-processor/white-noise-processor.js');
+    const whiteNoiseNode = new AudioWorkletNode(audioContext, 'white-noise-processor');
+    const whiteNoiseNodeGain = audioContext.createGain();
+    whiteNoiseNodeGain.gain.value = val;
+    whiteNoiseNode.connect(whiteNoiseNodeGain);
+    whiteNoiseNodeGain.connect(audioContext.destination);
+}
