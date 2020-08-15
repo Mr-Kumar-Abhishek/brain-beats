52f112ea073cdea7f65158cfc4e84c4a1d78018f
Abhishek Kumar <mr.kumar.abhishek@email.com>
2020-08-15 21:02:34 +0530

    set temp test of white noise processor

--- noise-processor/noise-processor.js
+++ noise-processor/noise-processor.js
@@ -1,4 +1,7 @@
 const audioContext = new AudioContext()
 await audioContext.audioWorklet.addModule('white-noise-processor.js')
-const whiteNoiseNode = new AudioWorkletNode(audioContext, 'white-noise-processor')
-whiteNoiseNode.connect(audioContext.destination)
+const whiteNoiseNode = new AudioWorkletNode(audioContext, 'white-noise-processor');
+const whiteNoiseNodeGain = audioContext.createGain();
+whiteNoiseNodeGain.gain.value = 0.0;
+whiteNoiseNode.connect(whiteNoiseNodeGain);
+whiteNoiseNodeGain.connect(audioContext.destination);
