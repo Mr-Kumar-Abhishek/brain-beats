4a2d81bfb82f06d700806bfce3a90ffc5de7ed45
Abhishek Kumar <mr.kumar.abhishek@email.com>
2021-06-06 00:56:52 +0530

    include workbox service worker

--- js/serviceLoader.js
+++ js/serviceLoader.js
@@ -1,11 +1,7 @@
-window.addEventListener('load', ()=>{
-
-  if('serviceWorker' in navigator){
-    try {
-      navigator.serviceWorker.register('serviceWorker.js');
-      console.log("Service Worker Registered");
-    } catch (error) {
-      console.log("Service Worker Registration Failed");
-    }
-  }
-});
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+   navigator.serviceWorker.register('sw.js').then( () => {
+    console.log('Service Worker Registered')
+   })
+ })
+}
