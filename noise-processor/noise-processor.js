059d91d448fbaec4bdca1dc1c9020714acaf936d
Abhishek Kumar <mr.kumar.abhishek@email.com>
2021-06-06 10:50:55 +0530

    implement pink and brown noise

--- noise-processor/noise-processor.js
+++ noise-processor/noise-processor.js
@@ -1,7 +1,11 @@
 
 var audioContext;
-var whiteNoiseNodel
+var whiteNoiseNode;
 var whiteNoiseNodeGain;
+var pinkNoiseNode;
+var pinkNoiseNodeGain;
+var brownNoiseNode;
+var brownNoiseNodeGain;
 
 async function white_noise_process(val) {
     audioContext = new AudioContext();
@@ -15,4 +19,32 @@ async function white_noise_process(val) {
 
 function stopWhiteNoise(){
     whiteNoiseNodeGain.disconnect();
+}
+
+async function pink_noise_process(val) {
+    audioContext = new AudioContext();
+    await audioContext.audioWorklet.addModule('noise-processor/pink-noise-processor.js');
+    pinkNoiseNode = new AudioWorkletNode(audioContext, 'pink-noise-processor');
+    pinkNoiseNodeGain = audioContext.createGain();
+    pinkNoiseNodeGain.gain.value = val;
+    pinkNoiseNode.connect(pinkNoiseNodeGain);
+    pinkNoiseNodeGain.connect(audioContext.destination);
+}
+
+function stopPinkNoise() {
+    pinkNoiseNodeGain.disconnect();
+}
+
+async function brown_noise_process(val) {
+    audioContext = new AudioContext();
+    await audioContext.audioWorklet.addModule('noise-processor/brown-noise-processor.js');
+    brownNoiseNode = new AudioWorkletNode(audioContext, 'brown-noise-processor');
+    brownNoiseNodeGain = audioContext.createGain();
+    brownNoiseNodeGain.gain.value = val;
+    brownNoiseNode.connect(brownNoiseNodeGain);
+    brownNoiseNodeGain.connect(audioContext.destination);
+}
+
+function stopBrownNoise() {
+    brownNoiseNodeGain.disconnect();
 }
