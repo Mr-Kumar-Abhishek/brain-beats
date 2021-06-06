98a9f4f8999381544be9b1fe29f285080fa1a777
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2021-06-06 10:50:55 +0530

    implement pink and brown noise

--- noise-processor/brown-noise-processor.js
+++ noise-processor/brown-noise-processor.js
@@ -1,5 +1,6 @@
 class BrownNoiseProcessor extends AudioWorkletProcessor {
     process (inputs, outputs, parameters) {
+      var lastOut = 0.0;
       const output = outputs[0];
       output.forEach(channel => {
         for (let i = 0; i < channel.length; i++) {
