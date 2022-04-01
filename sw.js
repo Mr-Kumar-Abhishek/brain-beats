69d97e89cf1f9f0c5f8e6bcf794c5dffc68b6a4c
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2022-04-01 19:56:33 +0530

    fix workbox error

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ if (workbox) {
         new workbox.strategies.CacheFirst({
             cacheName: "images",
             plugins: [
-                new workbox.expiration.Plugin({
+                new workbox.expiration.ExpirationPlugin({
                     maxEntries: 50,
                     maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
                 })
