b6973ce35e56e083bb72ad954c2868a82cb73114
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2022-09-27 22:21:57 +0530

    separate search function to different file

---
+++ js/data.js
@@ -0,0 +1,36 @@
+const dataCards = document.querySelector(".data-cards");
+const dataContainer = document.querySelector(".data-container");
+const searchInput = document.querySelector(".searching");
+
+
+let dPresets = [];
+searchInput.addEventListener("input", (e)=> {
+  const value  = e.target.value.toLowerCase(); 
+  dPresets.forEach(dPreset => {
+     console.log(dPreset);
+    const isVisible = dPreset.dTitle.toLowerCase().includes(value) || dPreset.dDesc.toLowerCase().includes(value);
+    dPreset.element.classList.toggle("d-none", !isVisible)
+  });
+});
+
+fetch(jsonData)
+.then(res => res.json())
+.then(data => {
+ dPresets = data.map(dataPreset => {
+  const dataNode = dataCards.content.cloneNode(true).children[0];
+  const dataTitle = dataNode.querySelector(".card-title");
+  const dataDescription = dataNode.querySelector(".card-text");
+  const dataPlay = dataNode.querySelector(".play");
+  const dataStop = dataNode.querySelector(".stop");
+  dataTitle.textContent = dataPreset.data_name;
+  dataDescription.textContent = dataPreset.data_description;
+  dataPlay.setAttribute("onclick", dataPreset.data_start);
+  dataStop.setAttribute("onclick", dataPreset.data_stop);
+  dataContainer.append(dataNode);
+  return {dTitle: dataPreset.data_name, dDesc: dataPreset.data_description, element: dataNode};
+  });
+});
+
+$("#search-form").submit(function(e) {
+  e.preventDefault();
+});
