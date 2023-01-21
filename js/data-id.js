5c5034f6025d63b09816395b56837b6d4c01c8bb
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2023-01-22 01:56:04 +0530

    include data id for pure tones

--- js/data-id.js
+++ js/data-id.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // Read JSON file
-const jsonData = JSON.parse(fs.readFileSync('../json/noise.json', 'utf-8'));
+const jsonData = JSON.parse(fs.readFileSync('../json/puretones.json', 'utf-8'));
 
 // Function to generate unique ID
 function generateID() {
@@ -14,4 +14,4 @@ jsonData.forEach(obj => {
 });
 
 // Write updated JSON to file
-fs.writeFileSync('../json/noise.json', JSON.stringify(jsonData));
+fs.writeFileSync('../json/puretones.json', JSON.stringify(jsonData));
