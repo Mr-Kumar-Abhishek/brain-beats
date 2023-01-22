03740de52f11cd6f4dbf9bfe97c7f594df0aa377
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2023-01-23 02:31:18 +0530

    minor bug fix

---
+++ js/favs-data.js
@@ -0,0 +1,24 @@
+// get favorites from local storage or empty array
+var favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+// register click event listener
+document.querySelector('.data-container').addEventListener('click', function(e) {
+  var item = $(e.target);
+  var id = item.attr("id");
+  var index = favorites.indexOf(id);
+  // return if target doesn't have an id (shouldn't happen)
+  if (!id) return;
+  // item is not favorite
+  if (index == -1) {
+    favorites.push(id);
+    item.addClass("faved");
+  // item is already favorite
+  } else {
+    favorites.splice(index, 1);
+    item.removeClass("faved");
+  }
+  // store array in local storage
+  localStorage.setItem('favorites', JSON.stringify(favorites));
+});
+
+// local storage stores strings so we use JSON to stringify for storage and parse to get out of storage
