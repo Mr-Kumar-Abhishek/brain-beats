04eac11bb201815b616d13bb37a744f13def81f3
Abhishek Kumar <mr.kumar.abhishek@email.com>
2023-01-24 05:37:24 +0530

    set pink noise async function

--- js/main.js
+++ js/main.js
@@ -234,36 +234,16 @@ function play_white_noise() {
   }
 }
 
-function play_pink_noise() {
-  if (pink_noise_flag == 0) {
-      pink_noise_flag = 1;
-      bufferSize = 4096;
-      var pinkNoiseBuffer = audioCtx.createBuffer(1, bufferSize, audioCtx.sampleRate);
-      var output = pinkNoiseBuffer.getChannelData(0);
-      var b0, b1, b2, b3, b4, b5, b6;
-      b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
-      for (var i = 0; i < bufferSize; i++) {
-          var white = Math.random() * 2 - 1;
-          b0 = 0.99886 * b0 + white * 0.0555179;
-          b1 = 0.99332 * b1 + white * 0.0750759;
-          b2 = 0.96900 * b2 + white * 0.1538520;
-          b3 = 0.86650 * b3 + white * 0.3104856;
-          b4 = 0.55000 * b4 + white * 0.5329522;
-          b5 = -0.7616 * b5 - white * 0.0168980;
-          output[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
-          output[i] *= 0.11; // (roughly) compensate for gain
-          b6 = white * 0.115926;
-      }
-      pink_noise_node = audioCtx.createBufferSource();
-      pink_noise_node.buffer = pinkNoiseBuffer;
-      pink_noise_volume = audioCtx.createGain();
-      pink_noise_node.connect(pink_noise_volume);
-      pink_noise_volume.connect(audioCtx.destination);
-      pink_noise_volume.gain.value = volume_set();
-      pink_noise_node.start();
-  } else {
-      stop_pink_noise();
-      play_pink_noise();
+async function play_pink_noise() {
+  if (boolPink == 0 ){
+    boolPink = 1;
+    audioContext = new AudioContext();
+    await audioContext.audioWorklet.addModule('noise-processor/pink-noise-processor.js');
+    pinkNoiseNode = new AudioWorkletNode(audioContext, 'pink-noise-processor');
+    pinkNoiseNodeGain = audioContext.createGain();
+    pinkNoiseNodeGain.gain.value = volume_set();
+    pinkNoiseNode.connect(pinkNoiseNodeGain);
+    pinkNoiseNodeGain.connect(audioContext.destination);
   }
 }
 
@@ -330,9 +310,10 @@ function stop_white_noise() {
 }
 
 function stop_pink_noise() {
-  pink_noise_flag = 0;
-  pink_noise_node.stop();
-  pink_noise_volume.disconnect(audioCtx.destination);
+  if (boolPink == 1 ){
+    boolPink = 0;
+    pinkNoiseNodeGain.disconnect();
+  }
 }
 
 function play_monaural_generator(){
