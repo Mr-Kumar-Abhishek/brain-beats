196c34672a9acd7d138f7c1f01841c6ef0a28b91
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2023-01-26 05:25:59 +0530

    check for double tone at the last

--- js/main.js
+++ js/main.js
@@ -367,7 +367,7 @@ function live_volume_set(){
    } 
     
   }else if (monaural_flag == 1 || binaural_flag == 1 || 
-            double_tone_flag == 1 || sq_monaural_flag == 1){
+            sq_monaural_flag == 1 double_tone_flag == 1 ){
     
     if(volume_1.gain.value != undefined && volume_2.gain.value != undefined) {
       volume_1.gain.value = volume_set();
