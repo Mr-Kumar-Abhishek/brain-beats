c7b71738e90efd1e0678c086528c2175656f814f
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2023-08-30 15:02:48 +0530

    add website tracking

--- js/google-ads.js
+++ js/google-ads.js
@@ -4,4 +4,7 @@
   gtag('js', new Date());
 
   gtag('config', 'AW-645983156');
+
+  /* Event snippet for Website traffic (1) conversion page */
   
+gtag('event', 'conversion', {'send_to': 'AW-645983156/TNfMCK-auNoYELTXg7QC'});
