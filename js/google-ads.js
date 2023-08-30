f0688e4752a79f3c97eea794384c3c723db612dd
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2023-08-30 14:59:18 +0530

    add google ads

--- js/google-ads.js
+++ js/google-ads.js
@@ -1,5 +1,7 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
 
-gtag('config', 'AW-645983156');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-645983156');
+  
