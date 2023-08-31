b7f841742a64804b2e9ea6e07de83505d082c54d
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2023-08-31 09:42:39 +0530

    add adsterra code seperately

---
+++ js/adsterra.js
@@ -0,0 +1,8 @@
+atOptions = {
+    'key' : '7caff1015978f22d388e2e6204d9d3f4',
+    'format' : 'iframe',
+    'height' : 50,
+    'width' : 320,
+    'params' : {}
+  };
+  document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitablecreativeformat.com/7caff1015978f22d388e2e6204d9d3f4/invoke.js"></scr' + 'ipt>');
