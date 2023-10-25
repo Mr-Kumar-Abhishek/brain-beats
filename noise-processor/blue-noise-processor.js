16b388b1f54b97630e76c6778d2805b7fe1dab17
Abhishek Kumar <mr.kumar.abhishek@email.com>
2023-10-25 05:53:22 +0530

    make algorithm for blue noise

--- noise-processor/blue-noise-processor.js
+++ noise-processor/blue-noise-processor.js
@@ -1,9 +1,14 @@
 class WhiteNoiseProcessor extends AudioWorkletProcessor {
     process (inputs, outputs, parameters) {
-      const output = outputs[0]
+      const output = outputs[0];
+      // Generate blue noise samples
+      let lastValue = 0;
       output.forEach(channel => {
         for (let i = 0; i < channel.length; i++) {
-          channel[i] = Math.random() * 2 - 1
+          var whiteNoise = Math.random() * 2 - 1;
+          var blueNoise = (whiteNoise + lastValue) / 2;
+          channel[i] = blueNoise;
+          lastValue = blueNoise;
         }
       })
       return true
