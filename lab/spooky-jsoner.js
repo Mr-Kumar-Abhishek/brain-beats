0e1b565788c669a42dc4a8e9a6bf4b424da7464b
Abhishek Kumar <mr.kumar.abhishek@email.com>
2024-01-16 04:37:02 +0530

    sanitize spooky alts main

--- lab/spooky-jsoner.js
+++ lab/spooky-jsoner.js
@@ -15,16 +15,19 @@ function generateId() {
 function convertLine(line) {
   // Split the line by quotation mark
   let parts = line.split('"');
+  // console.log(parts);
   // Get the name and type from the first part
   let nameAndType = parts[0].split(',');
   // console.log(nameAndType);
   let name = nameAndType.slice(0, -1).join(','); // Join the name parts with comma
   // let type = nameAndType[nameAndType.length - 1];
-  let type = parts[2].slice(1, -2);
-  // console.log(type);
+  let type = parts[2];
+  if (type == undefined) {console.log(parts)}
+  //console.log(type);
   // Get the frequencies from the second part
+  /*
   let frequencies = parts[1].split(',').filter(x => x); // Remove any empty strings
-  console.log(frequencies);
+  // console.log(frequencies);
   // Construct the json object
   let json = {};
   json.data_name = name + ' ' + type + ' 3D frequencies';
@@ -33,6 +36,7 @@ function convertLine(line) {
   json.data_stop = 'stop_' + type + '_3d();';
   json.data_id = generateId();
   return json;
+  */
 }
 
 // Define a function to convert a text file to a json file
@@ -51,6 +55,7 @@ function convertFile(inputFile, outputFile) {
       let json = convertLine(line);
       jsonArray.push(json);
     }
+    /*
     // Write the array to the output file as a json string
     fs.writeFile(outputFile, JSON.stringify(jsonArray, null, 2), 'utf8', (err) => {
       if (err) {
@@ -59,8 +64,9 @@ function convertFile(inputFile, outputFile) {
       }
       console.log('Conversion successful!');
     });
+    */
   });
 }
 
 // Call the function with the input and output file names
-convertFile('spooky-cafl-frequency.txt', 'cafl-freq.json');
+convertFile('spooky-cust-frequency.txt', 'cust-freq.json');
