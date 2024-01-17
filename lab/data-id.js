d54a18f225c5267d0731517a6b5364356c354688
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2024-01-17 09:46:28 +0530

    get data ID generator to the lab

---
+++ lab/data-id.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+// Read JSON file
+const jsonData = JSON.parse(fs.readFileSync('../json/rife-monaural-freq.json', 'utf-8'));
+
+// Function to generate unique ID
+function generateID() {
+  return Math.random().toString(36).substr(2, 9);
+}
+
+// Add unique ID to each object in JSON
+jsonData.forEach(obj => {
+  obj.data_id = generateID();
+});
+
+// Write updated JSON to file
+fs.writeFileSync('../json/rife-monaural-freq.json', JSON.stringify(jsonData));
