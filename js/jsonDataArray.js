acda107f22cb524e2a5e90f1e482c89daed3bc0a
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2024-12-29 16:51:35 +0530

    set one file for jsonDataArray

---
+++ js/jsonDataArray.js
@@ -0,0 +1,15 @@
+const jsonData0 = "./json/noise.json";
+const jsonData1 = "./json/solfeggio.json";
+const jsonData2 = "./json/puretones.json";
+const jsonData3 = "./json/binaurals.json";
+const jsonData4 = "./json/monaurals.json";
+const jsonData5 = "./json/sq_monaurals.json";
+const jsonData6 = "./json/isochronic.json";
+const jsonData7 = "./json/dreamachines.json";
+const jsonData8 = "./json/angel.json";
+const jsonData9 = "./json/rife3Dfreq.json";
+const jsonData10 = "./json/rife-monaural-freq.json";
+const jsonData11 = "./json/rife-frequencies-cafl-xref.json";
+const jsonData12 = "./json/alt-freq-auto.json";
+const jsonData13 = "./json/alt-freq-monaural.json";
+const jsonDataArray = [jsonData0, jsonData1, jsonData2, jsonData3, jsonData4, jsonData5, jsonData6, jsonData7, jsonData8, jsonData9, jsonData10, jsonData11, jsonData12, jsonData13];
