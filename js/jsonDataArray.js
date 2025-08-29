06996050cd50b482d6208a60e4eb65fd2e459814
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2025-08-29 15:36:33 +0530

    add bio freq auto json list in total list

--- js/jsonDataArray.js
+++ js/jsonDataArray.js
@@ -12,4 +12,5 @@ const jsonData10 = "./json/rife-monaural-freq.json";
 const jsonData11 = "./json/rife-frequencies-cafl-xref.json";
 const jsonData12 = "./json/alt-freq-auto.json";
 const jsonData13 = "./json/alt-freq-monaural.json";
-const jsonDataArray = [jsonData0, jsonData1, jsonData2, jsonData3, jsonData4, jsonData5, jsonData6, jsonData7, jsonData8, jsonData9, jsonData10, jsonData11, jsonData12, jsonData13];
+const jsonData14 = "./json/bio-freq-auto.json";
+const jsonDataArray = [jsonData0, jsonData1, jsonData2, jsonData3, jsonData4, jsonData5, jsonData6, jsonData7, jsonData8, jsonData9, jsonData10, jsonData11, jsonData12, jsonData13, jsonData14];
