fa33c7934228156876af9fd0968028266d0b9d7a
Abhishek Kumar <mr.kumar.abhishek@outlook.in>
2025-08-29 21:06:49 +0530

    stage for generation of new data ids for bio monaural freq

--- lab/data-id.js
+++ lab/data-id.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // Read JSON file
-const jsonData = JSON.parse(fs.readFileSync('../json/alt-freq-monaural.json', 'utf-8'));
+const jsonData = JSON.parse(fs.readFileSync('../json/bio-monaural-freq.json', 'utf-8'));
 
 // Function to generate unique ID
 function generateID() {
@@ -14,4 +14,4 @@ jsonData.forEach(obj => {
 });
 
 // Write updated JSON to file
-fs.writeFileSync('../json/alt-freq-monaural.json', JSON.stringify(jsonData));
+fs.writeFileSync('../json/bio-monaural-freq.json', JSON.stringify(jsonData));
